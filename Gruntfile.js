a598e9e48284bcfd99f0218f6423fc9bc94e2301
Elena <elena@Elenas-MacBook-Pro.local>
2017-10-16 16:41:31 +0300

    changes3

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,6 +31,10 @@ module.exports = function(grunt) {
         options: {
           server: "build/",
           watchTask: true,
+          notify: false,
+          open: true,
+          cors: true,
+          ui: false
         }
       }
     },
